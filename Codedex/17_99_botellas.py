ca478a4ed1858846061e71a856b54602a38b4cf7
Krezitech <krezipedtech@gmail.com>
2023-02-11 19:05:11 -0600

    99 17_99_botellas

---
+++ Codedex/17_99_botellas.py
@@ -0,0 +1,7 @@
+
+for x in range(100,-1,-1):           # El rango empieza en 100, hasta 0, restando -1 cada vez
+  print(x,"bottles of beer on the wall" )
+  print(x,"bottles of beer" )
+  print("Take one down, pass it around")
+
+print("And now you are out of beers")
