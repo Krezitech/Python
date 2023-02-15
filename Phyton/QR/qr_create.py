18fd69b98f4485e13c61896be8234e51fa9ed939
Krezitech <krezipedtech@gmail.com>
2023-02-15 16:24:22 -0600

    actualizacion

--- Phyton/QR/qr_create.py
+++ Phyton/QR/qr_create.py
@@ -1,13 +1,12 @@
 import qrcode                                                       # importa la librería para poder hacer el QR
 
-website_link = "https://www.instagram.com/kreziped"                 # Ponemos la url a abrir con el qr
-
-llamar = call(["+"+522299068005])
+website_link = 'https://www.youtube.com/'               # Ponemos la url a abrir con el qr
 
 qr = qrcode.QRCode(version = 3, box_size = 10, border = 5)          # Cambia los atributos, version es que "tan complejo será", bozsize "tamaño del qr", border es el borde "fuera del qr"    
-
-qr.add_data(llamar)                                           # toma la info del website link para crear el qr
+qr.add_data(website_link)                                         # toma la info del website link para crear el qr
 qr.make()                                                           # Crea el qr
 
 img = qr.make_image(fill_color = 'white', back_color = 'black')     # Ponemos los colores a usar
 img.save('llamada.png')                                           # Como se guarda la imagen y donde
+
+
