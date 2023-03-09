47a07252b6bd16e7d22d78ec6f7b5d9dd199c5c6
Krezitech-Win <krezipedtech@gmail.com>
2023-03-09 15:03:30 -0600

    00:14:54

--- Curso 2 Moudev Intermedio/00_dates.py
+++ Curso 2 Moudev Intermedio/00_dates.py
@@ -8,7 +8,17 @@ print("#################      1      ###################")
 print("#################################################")
 print(" ")
 
-import datetime                 # Se agrega el "módulo" de datetime
+from datetime import datetime               # Se agrega el "módulo" de "datetime"
 
-now = datetime.datetime
+now = datetime.now()
 
+print(now.year)
+print(now.month)
+print(now.day)
+print(now.hour)
+print(now.minute)
+print(now.second)
+
+timestamp = now.timestamp()
+
+print(timestamp)
